cd95154601eb4af7ede108c99661fa515d28728a
pillar <pillar@pillars-MacBook-Pro.local>
2019-06-20 16:26:24 +0800

    update-readme

--- README.md
+++ README.md
@@ -8,3 +8,19 @@ $ cd your-project
 $ npx ts-react-helper init
 $ yarn install
 ```
+
+查看 git status，不需要修改的文件自行删除
+
+## 其它参数
+
+查看参数:
+
+```sh
+$ npm ts-react-helper init --helper
+```
+
+修改 src 路径:
+
+```sh
+$ npm ts-react-helper init --src ./pages
+```
--- bin.js
+++ bin.js
@@ -22,8 +22,7 @@ const packageNeed = {
     lib: 'tscu example/src/lib --outDir lib',
     'lint-ci': 'lint-staged',
   },
-  dependencies: {
-    deepmerge: '^3.2.1',
+  devDependencies: {
     'http-proxy-middleware': '^0.19.1',
     husky: '^2.4.1',
     'lint-staged': '^8.2.1',
@@ -38,14 +37,19 @@ function copyOut(file, out = './') {
   fs.copyFileSync(binPwd(file), pwd(out, file));
 }
 
+let src = 'src';
+
 for (let i = 0; i++; i < argv.length) {
-  if (argv[i] === 'init') {
-    copyOut('tslint.json');
-    copyOut('setupProxy', 'src');
-    copyOut('LICENSE');
-    copyOut('.npmignore');
-    let package = require(pwd('package.json'));
-    package = deepmerge(package, packageNeed);
-    fs.writeFileSync(pwd('package.json'), JSON.stringify(package, null, 2), 'utf8');
+  if (argv[i] === '--src') {
+    src = argv[i + 1];
   }
 }
+
+copyOut('tslint.json');
+copyOut('setupProxy.js', src);
+copyOut('LICENSE');
+fs.copyFileSync(binPwd('.gitignore-copy'), pwd('.gitignore'));
+fs.copyFileSync(binPwd('.npmignore-copy'), pwd('.npmignore'));
+let package = require(pwd('package.json'));
+package = deepmerge(package, packageNeed);
+fs.writeFileSync(pwd('package.json'), JSON.stringify(package, null, 2), 'utf8');
